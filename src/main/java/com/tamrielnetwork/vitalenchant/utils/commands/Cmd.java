ba4ba3c8b7be8c7052f57ebac00a14f0bd35fa4c
TamrielNetwork <contact@tamriel.me>
2022-03-07 05:57:39 +0100

    Update Copyright to feature contributors

--- src/main/java/com/tamrielnetwork/vitalenchant/utils/commands/Cmd.java
+++ src/main/java/com/tamrielnetwork/vitalenchant/utils/commands/Cmd.java
@@ -1,6 +1,6 @@
 /*
  * VitalEnchant is a Spigot Plugin that gives players the ability to enchant items.
- * Copyright © 2022 Leopold Meinel
+ * Copyright © 2022 Leopold Meinel & contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
